8126bf464303b287806cf25b8faa96628210fbe6
Amit <nikumbhamit0000@gmail.com>
2026-06-30 08:39:32 +0530

    Updated app.py for deployment

--- app.py
+++ app.py
@@ -13,36 +13,41 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    try:
+        data = {
+            "area": [float(request.form["area"])],
+            "bedrooms": [int(request.form["bedrooms"])],
+            "bathrooms": [int(request.form["bathrooms"])],
+            "stories": [int(request.form["stories"])],
+            "mainroad": [request.form["mainroad"]],
+            "guestroom": [request.form["guestroom"]],
+            "basement": [request.form["basement"]],
+            "hotwaterheating": [request.form["hotwaterheating"]],
+            "airconditioning": [request.form["airconditioning"]],
+            "parking": [int(request.form["parking"])],
+            "prefarea": [request.form["prefarea"]],
+            "furnishingstatus": [request.form["furnishingstatus"]]
+        }
 
-    data = {
-        "area": [float(request.form["area"])],
-        "bedrooms": [int(request.form["bedrooms"])],
-        "bathrooms": [int(request.form["bathrooms"])],
-        "stories": [int(request.form["stories"])],
-        "mainroad": [request.form["mainroad"]],
-        "guestroom": [request.form["guestroom"]],
-        "basement": [request.form["basement"]],
-        "hotwaterheating": [request.form["hotwaterheating"]],
-        "airconditioning": [request.form["airconditioning"]],
-        "parking": [int(request.form["parking"])],
-        "prefarea": [request.form["prefarea"]],
-        "furnishingstatus": [request.form["furnishingstatus"]]
-    }
+        input_df = pd.DataFrame(data)
 
-    input_df = pd.DataFrame(data)
+        pipeline = PredictionPipeline()
 
-    pipeline = PredictionPipeline()
+        prediction = pipeline.predict(input_df)
 
-    prediction = pipeline.predict(input_df)
+        predicted_price = prediction[0]
 
-    return render_template(
-        "result.html",
-        prediction=f"₹ {prediction[0]:,.2f}",
-        model_name="Linear Regression",
-        accuracy="85.29%",
-        features=12
-    )
+        return render_template(
+            "result.html",
+            prediction=f"₹ {predicted_price:,.2f}",
+            model_name="Linear Regression",
+            accuracy="65.29%",
+            features=12
+        )
+
+    except Exception as e:
+        return f"<h2>Error:</h2><p>{str(e)}</p>"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
